9003a23db9d5895e8ba1a53a8b1b14eef12fd983
Будников Сергей Владимирович <sv_budnikov@bingo-boom.com>
2019-01-22 16:21:44 +0300

    fix shapeos_run.sh

--- shapeos_run.sh
+++ shapeos_run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [ "${SSH_CONNECTION}x" == "x" ]
+
+
+COUNT=`echo ${SSH_CONNECTION} | wc -c`
+
+if [ $COUNT -eq 0 ]
 then
 	echo "[-] Get update for ShapeOS"
         docker pull bres/shape-os
         echo "[-] Run ShapeOS"
-        docker run -it -e USER=${USER} -v /opt/user_data:/home/ -v /var/run/docker.sock:/var/run/docker.sock -v /dev/:/dev/ --privileged --ipc host --net host bres/shape-os	
+        docker run -it -e USER=${USER} -v /opt/user_data:/home/ -v /var/run/docker.sock:/docker.sock -v /dev/:/dev/ --privileged --ipc host --net host bres/shape-os	
 else
 	echo Welcome
 fi
