095a8cf1a587ce41d48be44c747f111f72abe94c
Sergey Budnikov <s.budnikov@escalar.ru>
2019-01-27 22:41:13 +0300

    update shapeos_run.sh

--- shapeos_run.sh
+++ shapeos_run.sh
@@ -13,7 +13,7 @@ then
     	     docker build -t bres/shape-os /opt/shapeos/
 	fi
         echo "[-] Run ShapeOS"
-        docker run -it -e USER=${USER} -v /opt/user_data:/home/ -v /var/run/docker.sock:/docker.sock -v /dev/:/dev/ --privileged --ipc host --net host bres/shape-os	
+        docker run -it -e USER=${USER} -v /opt/shapeos:/custom  -v /opt/user_data:/home/ -v /var/run/docker.sock:/docker.sock -v /dev/:/dev/ --privileged --ipc host --net host bres/shape-os	
 else
 	echo Welcome
 fi
