5f1c0752b974e880404f705b5977d6b722b40bdc
Sergey Budnikov <s.budnikov@escalar.ru>
2019-01-27 22:39:34 +0300

    update shapeos_run.sh

--- shapeos_run.sh
+++ shapeos_run.sh
@@ -3,6 +3,11 @@ if [ "${SSH_CONNECTION}x" == "x" ]
 then
 	echo "[-] Get update for ShapeOS"
         docker pull bres/shape-os
+        if [ -f /opt/shapeos/Dockerfile ]
+	then
+             echo "[-] Run build custom image"
+    	     docker build -t bres/shape-os /opt/shapeos/
+	fi
         echo "[-] Run ShapeOS"
         docker run -it -e USER=${USER} -v /opt/user_data:/home/ -v /var/run/docker.sock:/var/run/docker.sock -v /dev/:/dev/ --privileged --ipc host --net host bres/shape-os	
 else
