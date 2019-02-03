a2d7f39bc3977589a288f57544dcd7ee6784929a
Sergey Budnikov <s.budnikov@escalar.ru>
2019-02-03 16:16:54 +0300

    fix shapeos_run.sh

--- shapeos_run.sh
+++ shapeos_run.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
-
+if [ ${USER} == root ]
+then
+	echo "Your root, hi!"
+	return 
+fi
 COUNT=`echo ${SSH_CONNECTION} | wc -c`
-
+DOCKER_RUN="docker run -it -e USER=${USER} -v /opt/shapeos:/custom  -v /opt/user_data:/opt/user_data -v /var/run/docker.sock:/docker_on_host.sock --privileged --ipc host --net host"
 if [ $COUNT -eq 1 ]
 then
 	echo "[-] Get update for ShapeOS"
@@ -10,10 +14,17 @@ then
         if [ -f /opt/shapeos/Dockerfile ]
 	then
              echo "[-] Run build custom image"
-    	     docker build -t bres/shape-os /opt/shapeos/
+    	     docker build -t bres/shape-os:local /opt/shapeos/
 	fi
-        echo "[-] Run ShapeOS"
-        docker run -it -e USER=${USER} -v /opt/shapeos:/custom  -v /opt/user_data:/home/ -v /var/run/docker.sock:/docker.sock -v /dev/:/dev/ --privileged --ipc host --net host bres/shape-os	
+	if [ "x`docker images bres/shape-os:local -q`" == "x" ]
+	then
+             echo "[-] Run ShapeOS latest"
+             exec $DOCKER_RUN bres/shape-os:latest
+	else 
+             echo "[-] Run ShapeOS local"
+             exec $DOCKER_RUN bres/shape-os:local
+	fi
 else
 	echo Welcome
 fi
+
